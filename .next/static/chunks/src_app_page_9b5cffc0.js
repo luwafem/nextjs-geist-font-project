b96e83244ce3bc3a74b183d744aaced60d2da7e3
luwafem <oluwafemiemmanuelayedogbon@gmail.com>
2025-08-05 23:05:53 +0100

    Add .next/static/chunks/src_app_page_9b5cffc0.js 1754431552354

---
+++ .next/static/chunks/src_app_page_9b5cffc0.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm_bf7383cb.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm_19aa928b._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_10e2c709._.js",
+  "static/chunks/src_f1168617._.js"
+],
+    source: "dynamic"
+});
